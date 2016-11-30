7eca2d16372fd6771220f03189aaa0659a212a30
taehyun.park <taehyun.park@navercorp.com>
2016-12-01 01:23:11 +0900

    모델 생성및 메인 UI 작업중

---
+++ Triplan/메인뷰컨트롤러.swift
@@ -0,0 +1,15 @@
+//
+//  메인뷰컨트롤러.swift
+//  Triplan
+//
+//  Created by Naver on 2016. 12. 1..
+//  Copyright © 2016년 Ben. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class 메인뷰컨트롤러: UIViewController {
+    
+}
