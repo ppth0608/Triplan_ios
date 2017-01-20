b744cc491f4297969bb4d28798e8b8656896e12e
Ben.park <ppth0608@naver.com>
2017-01-20 22:28:07 +0900

    SwiftLint — modify `conditional_returns_on_newline`

--- Triplan/공통_네비게이션뷰컨트롤러.swift
+++ Triplan/공통_네비게이션뷰컨트롤러.swift
@@ -26,7 +26,9 @@ extension 공통_네비게이션뷰컨ᄐ
 
 extension 공통_네비게이션뷰컨트롤러: UIGestureRecognizerDelegate {
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        guard let 네비게이션뷰컨트롤러 = self.navigationController else { return false }
+        guard let 네비게이션뷰컨트롤러 = self.navigationController else {
+            return false
+        }
 
         if 네비게이션뷰컨트롤러.topViewController is 메인_뷰컨트롤러 {
             performSegue(withIdentifier: "메뉴세그웨이", sender: nil)
